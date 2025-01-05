66d9023590ef1a3cb66b1b3c79ae6fb9160d915c
inoueyuji <sikogaku2593@gmail.com>
2025-01-05 12:35:43 +0900

    learn1

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,14 @@
 class TopsController < ApplicationController
+
   def index;end
-  
+
+  def create
+    @color = params[:color]
+      render turbo_stream: turbo_stream.replace(
+        'color-display',
+        partial: 'tops/display',
+        locals: { color: @color}
+    )
+  end
+
 end
